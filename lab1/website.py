9bb232424877682d422eb6064ce2b96d457c8662
server <uis.server>
2023-09-13 10:40:52 +0000

    fixed kubernetes deployment

--- lab1/website.py
+++ lab1/website.py
@@ -1,42 +1,71 @@
 # Python 3 server example
-from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 import redis
-import mysql.connector
+import cgi
 
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import parse_qs
 
+STARTING_BIT = "<body>"
+FORM = """
+<form method="post">
+    <label for="key">Key:</label>
+    <input type="text" id="key" name="key">
+    <br><br>
+
+    <label for="value">Value:</label>
+    <input type="text" id="value" name="value">
+    <br><br>
+
+    <input type="submit" value="Submit">
+</form>
+"""
 
 hostName = "0.0.0.0"
 serverPort = 80
 
+def redis_con():
+    return redis.Redis(host='redis', port=6379, username='default', password='', decode_responses=True)
+
+def get_html():
+    redis_client = redis_con() 
+
+    # Fetch all keys from Redis
+    keys = redis_client.keys("*")
+
+    html_list = "<ul>\n"
+    for key in keys:
+        html_list += f"<li>Key: {key}: Value: {redis_client.get(key)}</li>\n"
+    
+    return STARTING_BIT + FORM + html_list + "</body>"
+    
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
-
-        user_connection = redis.Redis(host='redis', port=6379, username='default', password='', decode_responses=True)
-        user_connection.ping()
-
-
-#        mydb = mysql.connector.connect(
-#        host="mysql",
-#        user="root",
-#        password=""
-#        )
-
-#        print(mydb) 
 
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes("</body>HELLO WORLD 3</html>", "utf-8"))
-        
-        
-    def get_DATA():
-        pass
+        self.wfile.write(bytes(get_html(), "utf-8"))
+
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length).decode('utf-8')
+        parsed_data = parse_qs(post_data)
+
+        user_connection = redis_con()
+        user_connection.set(parsed_data.get("key")[0], parsed_data.get("value")[0])
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(get_html(), "utf-8"))
+
+                
 
 if __name__ == "__main__":        
     webServer = HTTPServer((hostName, serverPort), MyServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
-
     try:
         webServer.serve_forever()
     except KeyboardInterrupt:
